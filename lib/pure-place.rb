7179edcef113eb14cf08bedbfcf63bc7ad2f1b8e
Matthew Lehner <matthewlehner@gmail.com>
2013-06-27 12:24:55 -0700

    rails stuff for asset pipeline.

--- lib/pure-place.rb
+++ lib/pure-place.rb
@@ -1,4 +1,16 @@
 require "pure-place/version"
 
+unless defined?(Sass)
+  require 'sass'
+end
+
 module PurePlace
+  if defined?(Rails) && defined?(Rails::Engine)
+    class Engine < ::Rails::Engine
+      require 'pure-place/engine'
+    end
+  else
+    Sass.load_paths << File.expand_path("../../app/assets/stylesheets", __FILE__)
+  end
+  
 end
---
+++ lib/pure-place/engine.rb
@@ -0,0 +1,5 @@
+module PurePlace
+  class Engine < Rails::Engine
+    # auto wire
+  end
+end
